feff5436289c241fff2974b395c9e41c3be1ac5a
Ben Basty <benbasty@yahoo.fr>
2021-10-13 19:51:05 +0800

    Create machine.java

---
+++ Stage 2/machine.java
@@ -0,0 +1,21 @@
+package machine;
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need:");
+        int cups = scanner.nextInt();
+        
+        int water = cups * 200;
+        int milk = cups * 50;
+        int coffebeans = cups * 15;
+        
+        System.out.println("For " + cups + " cups of coffee you will need:");
+        System.out.println(water + " ml of water");
+        System.out.println(milk + " ml of milk");
+        System.out.println(coffebeans + " g of coffee beans");
+        
+        
+    }
+}
